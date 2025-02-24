5edd7b28e30580d75ccdb173e43859eb53cfbbb3
nougatcat <105752568+nougatcat@users.noreply.github.com>
2025-02-24 03:26:29 +0300

    получение продуктов и категория из бд на главной странице 7:35

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import { TopBar, Container, Title, ProductsGroupList } from "@/components/shared";
 import { Filters } from "@/components/shared";
+import { prisma } from "@/prisma/prisma-client";
 
-export default function Home() {
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          items: true,
+        }
+      }
+    }
+  }) // позволяет получать данные так - categories[0].products[0].items
   return <>
     <Container className="mt-10">
       <Title text="Все пиццы" size="lg" className="font-extrabold"></Title>
     </Container>
-    <TopBar /> {/* Отдельно потому что топбар сам обернут в контейнер и не должен быть внутри этого контейнера */}
+    <TopBar categories={categories.filter((category) => category.products.length > 0)} /> {/* Отдельно потому что топбар сам обернут в контейнер и не должен быть внутри этого контейнера */}
 
     <Container className="pb-14 mt-10">
       <div className="flex gap-[80px]">
@@ -17,52 +28,18 @@ export default function Home() {
         {/* Список товаров */}
         <div className="flex-1">
           <div className="flex flex-col gap-16">
-            <ProductsGroupList title="Пиццы" items={[
-              {
-                id: 1,
-                name: "Бургер-пицца",
-                imageUrl: 'https://media.dodostatic.net/image/r:233x233/11ee7d61698827ee9b8db6d0aec53410.avif',
-                price: 550,
-                items: [{ price: 550 }]
-              },
-              {
-                id: 2,
-                name: "Бургер-пицца",
-                imageUrl: 'https://media.dodostatic.net/image/r:233x233/11ee7d61698827ee9b8db6d0aec53410.avif',
-                price: 550,
-                items: [{ price: 550 }]
-              },
-              {
-                id: 3,
-                name: "Бургер-пицца",
-                imageUrl: 'https://media.dodostatic.net/image/r:233x233/11ee7d61698827ee9b8db6d0aec53410.avif',
-                price: 550,
-                items: [{ price: 550 }]
-              },
-              {
-                id: 4,
-                name: "Бургер-пицца",
-                imageUrl: 'https://media.dodostatic.net/image/r:233x233/11ee7d61698827ee9b8db6d0aec53410.avif',
-                price: 550,
-                items: [{ price: 550 }]
-              },
-            ]} categoryId={1} />
-            <ProductsGroupList title="Завтрак" items={[
-              {
-                id: 5,
-                name: "Омлет",
-                imageUrl: 'https://media.dodostatic.net/image/r:233x233/11ee7970328e722d877398a9cf6e312b.avif',
-                price: 550,
-                items: [{ price: 550 }]
-              },
-              {
-                id: 6,
-                name: "Омлет",
-                imageUrl: 'https://media.dodostatic.net/image/r:233x233/11ee7970328e722d877398a9cf6e312b.avif',
-                price: 550,
-                items: [{ price: 550 }]
-              },
-            ]} categoryId={2} />
+            {
+              categories.map((category) => (
+                category.products.length > 0 && (
+                  <ProductsGroupList
+                    key={category.id}
+                    title={category.name}
+                    categoryId={category.id}
+                    items={category.products}
+                  />
+                )
+              ))
+            }
           </div>
         </div>
       </div>
