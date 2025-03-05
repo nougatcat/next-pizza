95f7c7b474ffd10b7b83d5629f7a9af626544d73
nougatcat <105752568+nougatcat@users.noreply.github.com>
2025-03-05 23:41:40 +0300

    9:18 выбор размера пиццы в модале

--- shared/components/shared/choose-pizza-form.tsx
+++ shared/components/shared/choose-pizza-form.tsx
@@ -1,8 +1,12 @@
+// 'use client'; 
+
 import { cn } from '@/shared/lib/utils';
 import React from 'react';
 import { PizzaImage } from './pizza-image';
 import { Title } from './title';
 import { Button } from '../ui';
+import { GroupVariants } from './group-variants';
+import { PizzaSize, pizzaSizes, PizzaType } from '@/shared/constants/pizza';
 
 interface Props {
     imageUrl: string;
@@ -21,16 +25,24 @@ export const ChoosePizzaForm: React.FC<Props> = ({
     onClickAdd,
     className
 }) => {
+    const [size, setSize] = React.useState<PizzaSize>(20)
+    const [type, setType] = React.useState<PizzaType>(1)
+
+
     const textDetails = 'шаблон 30 см, традиционное тесто 30'
     const totalPrice = 228
 
     return (
         <div className={cn(className, 'flex flex-1')}>
-            <PizzaImage imageUrl={imageUrl} size={30}/>
+            <PizzaImage imageUrl={imageUrl} size={size}/>
 
             <div className='w-[490px] bg-[#F7F6F5] p-7'>
                 <Title text={name} size='md' className='font-extrabold mb-1' />
                 <p className='text-gray-400'>{textDetails}</p>
+
+                <GroupVariants items={pizzaSizes} selectedValue={String(size)} 
+                    onClick={value => setSize(Number(value) as PizzaSize)} />
+
                 <Button
                     className='h-[55px] px-10 text-base rounded-[18px] w-full mt-10'
                 > Добавить в корзину за {totalPrice} ₽
---
+++ shared/constants/pizza.ts
@@ -0,0 +1,19 @@
+export const mapPizzaSize = {
+    20: 'Маленькая',
+    30: 'Средняя',
+    40: 'Большая',
+} as const;
+
+export const mapPizzaType = {
+    1: 'традиционная',
+    2: 'тонкая',
+} as const;
+
+export const pizzaSizes = Object.entries(mapPizzaSize).map(([value, name]) => ({
+    name,
+    value,
+}));
+console.log(pizzaSizes)
+
+export type PizzaSize = keyof typeof mapPizzaSize
+export type PizzaType = keyof typeof mapPizzaType
