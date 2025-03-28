7fbd76811b0a01b381f21bcab943b501d3c57e58
nougatcat <105752568+nougatcat@users.noreply.github.com>
2025-03-28 23:56:20 +0300

    packages update

--- shared/components/shared/address-input.tsx
+++ shared/components/shared/address-input.tsx
@@ -1,6 +1,6 @@
 
 'use client'
-import { dadataToken } from '@/shared/constants/dadata-token'
+import { dadataToken } from '@/shared/constants/api-tokens'
 import React from 'react'
 import { AddressSuggestions } from 'react-dadata'
 import 'react-dadata/dist/react-dadata.css'
