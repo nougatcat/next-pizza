aa13943b629347c20dbb44bfdb95a63cf934b266
nougatcat <105752568+nougatcat@users.noreply.github.com>
2025-03-29 06:38:40 +0300

    19:05 подключение ЮКАССА для редиректа на тестовый платеж для оплаты корзины (можно сделать не тестовый, если поменять api ключ) \ на данный момент при оплате НЕ УЧИТЫВАЮТСЯ НАЛОГИ И ДОСТАВКА - потом можно пофиксить

--- app/actions.ts
+++ app/actions.ts
@@ -4,7 +4,7 @@
 import { prisma } from "@/prisma/prisma-client";
 import { PayOrderTemplate } from "@/shared/components";
 import { CheckoutFormValues } from "@/shared/constants";
-import { sendEmail } from "@/shared/lib";
+import { createPayment, sendEmail } from "@/shared/lib";
 import { OrderStatus } from "@prisma/client";
 import { cookies } from "next/headers";
 
@@ -52,7 +52,7 @@ export async function createOrder(data: CheckoutFormValues) {
                 phone: data.phone,
                 address: data.address,
                 comment: data.comment,
-                totalAmount: userCart.totalAmount,
+                totalAmount: userCart.totalAmount, //! записывается цена без учета налога и доставки
                 status: OrderStatus.PENDING,
                 // items: JSON.stringify(userCart.items) //! Зачем тут json stringify? Он делает items строкой, а в бд ждут массив
                 items: userCart.items
@@ -76,13 +76,34 @@ export async function createOrder(data: CheckoutFormValues) {
         })
         // TODO: Сделать создание ссылки оплаты
 
+        const paymentData = await createPayment({
+            amount: order.totalAmount,
+            orderId: order.id,
+            description: 'Оплата заказа #' + order.id,
+        })
+
+        if (!paymentData) {
+            throw new Error('Payment data not found')
+        }
+
+        await prisma.order.update({
+            where: {
+                id: order.id
+            },
+            data: {
+                paymentId: paymentData.id
+            }
+        })
+
+        const paymentUrl = paymentData.confirmation.confirmation_url
+
         await sendEmail(data.email, 'Next Pizza / Оплатите заказ №' + order.id, PayOrderTemplate({
             orderId: order.id,
             totalAmount: order.totalAmount,
-            paymentUrl: 'https://www.youtube.com/watch?v=GUwizGbY4cc'
+            paymentUrl,
         }))
+        return paymentUrl
     } catch (err) {
         console.log('[CreateOrder] Server error', err)
     }
 }
-
--- shared/lib/index.ts
+++ shared/lib/index.ts
@@ -7,3 +7,4 @@ export {getCartDetails} from './get-cart-details'
 export {calcCartItemTotalPrice} from './calc-cart-item-total-price'
 export {updateCartTotalAmount} from './update-cart-total-amount';
 export {sendEmail} from './send-email'
+export {createPayment} from './create-payment'
