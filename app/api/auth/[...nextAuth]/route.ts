24b6b59a417ddc9f482c4ec7bd3e43e7ce81fa4a
nougatcat <105752568+nougatcat@users.noreply.github.com>
2025-04-06 13:37:18 +0300

    20:56 api Авторизация по почте и паролю сделана: доделывание next-auth api для входа через логин и пароль \ доделывание формы авторизации.

--- app/api/auth/[...nextAuth]/route.ts
+++ app/api/auth/[...nextAuth]/route.ts
@@ -1,15 +1,84 @@
+import { prisma } from './../../../../prisma/prisma-client';
 //? Эта директория и этот файл написаны по примеру из документации NextAuth
 
 import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github";
-
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { compare } from 'bcrypt';
 const authOptions = {
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID || '',
             clientSecret: process.env.GITHUB_SECRET || '',
+        }),
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                email: { label: "Email", type: "text" }, //чтобы next-auth понимал, какие поля будут передаваться, валидация
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) { //проверка данных по бд
+                if (!credentials) {
+                    return null
+                }
+                const values = {
+                    email: credentials.email
+                }
+
+                const findUser = await prisma.user.findFirst({
+                    where: values,
+                })
+
+                if (!findUser) {
+                    return null
+                }
+
+                const isPasswordValid = await compare(credentials.password, findUser.password) // сравнить хэш пароля введенного с тем что в бд
+
+                if (!isPasswordValid) {
+                    return null
+                }
+
+                if (!findUser.verified) { // пароль верен, но акк не активирован
+                    return null
+                }
+
+                return {
+                    id: String(findUser.id),
+                    email: findUser.email,
+                    name: findUser.fullName,
+                    role: findUser.role
+                } // инфа нужна в.т.ч. для дэшборда (админ панели), но самого дэшборда нет
+            }
         })
     ],
+    secret: process.env.NEXTAUTH_SECRET,
+    session: {
+        strategy: 'jwt',
+    },
+    callbacks: { //проверка jwt токена
+        async jwt({ token }) { //находим есть ли в бд такой емаил
+            const findUser = await prisma.user.findFirst({
+                where: {
+                    email: token.email
+                }
+            })
+            if (findUser) { //если есть, то вшиваем в токен инфу пользователя
+                token.id = String(findUser.id)
+                token.role = findUser.role
+                token.fullName = findUser.fullName
+                token.email = findUser.email
+            }
+            return token
+        },
+        session({session, token}) {
+            if (session?.user) {
+                session.user.id = token.id
+                session.user.role = token.role
+            }
+            return session
+        }
+    }
 }
 
 const handler = NextAuth(authOptions)
--- shared/components/shared/modals/auth-modal/auth-modal.tsx
+++ shared/components/shared/modals/auth-modal/auth-modal.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/shared/components/ui';
 import { Dialog, DialogContent } from '@/shared/components/ui/dialog';
 import { signIn } from 'next-auth/react';
 import React from 'react';
+import { LoginForm } from './forms/login-form';
 
 interface Props {
     open: boolean;
@@ -10,13 +11,23 @@ interface Props {
 }
 
 export const AuthModal: React.FC<Props> = ({ open, onClose }) => {
+    const [type, setType] = React.useState<'login' | 'register'>('login')
+
+    const onSwitchType = () => {
+        setType(type === 'login' ? 'register' : 'login')
+    }
+
     const handleClose = () => {
         onClose()
     }
     return (
         <Dialog open={open} onOpenChange={handleClose}>
             <DialogContent className='w-[450px] bg-white p-10'>
-                форма регистрации
+                {
+                    type === 'login'
+                        ? <LoginForm onClose={onClose} />
+                        : <h1>Регистрация</h1>
+                }
                 <hr />
                 {/* кнопки авторизации через гитхаб и через гугл */}
                 <div className='flex gap-2'>
@@ -50,6 +61,10 @@ export const AuthModal: React.FC<Props> = ({ open, onClose }) => {
                         Google/Gmail
                     </Button>
                 </div>
+
+                <Button variant='outline' onClick={onSwitchType} type='button' className='h-12'>
+                    {type !== 'login' ? 'Войти' : 'Регистрация'}
+                </Button>
             </DialogContent>
         </Dialog>
     );
