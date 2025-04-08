f86e6fd0e4cd985b2eb4b7c515dc23d92026166b
nougatcat <105752568+nougatcat@users.noreply.github.com>
2025-04-08 14:18:21 +0300

    Изменения перед сборкой

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,15 +1,12 @@
 import { prisma } from "@/prisma/prisma-client"
-import { authOptions } from "@/shared/constants/auth-options"
-// import { getUserSession } from "@/shared/lib/get-user-session"
-import { getServerSession } from "next-auth"
+import { getUserSession } from "@/shared/lib/get-user-session"
 import { NextResponse } from "next/server"
 
-export const dynamic = 'force-dynamic' //для багфикса
+export const dynamic = 'force-dynamic' //! есть баг с асинхронным контекстом на уровне next-js, поэтому билд может не работать в этом месте
 
-export async function GET(req: any, res: any) {
+export async function GET() {
     try {
-        // const user = await getUserSession() //! есть баг с асинхронным контекстом на уровне next-js, поэтому билд может не работать в этом месте
-        const user = await getServerSession(req, res, authOptions) // багфикс
+        const user = await getUserSession() 
         
         if (!user) {
             return NextResponse.json({ message: 'Вы не авторизованы'}, { status: 401 })
@@ -17,7 +14,7 @@ export async function GET(req: any, res: any) {
 
         const data = await prisma.user.findUnique({
             where: {
-                id: Number(user.user.id)
+                id: Number(user.id)
             },
             select: { //вытаскиваем эти поля из ответа (первые два, а третье - нет)
                 fullName: true,
